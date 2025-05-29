5a8cd05f0956214de3d26e446f3b92c32493f482
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-05-29 17:18:05 +0530

    Create variables.tf

---
+++ task2/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  type = string
+}
+
+variable "cidr_block" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "availability_zone_public_subnet" {
+  type = string
+}
+
+
+variable "publicSubnet_cidr_block" {
+  type = string
+}
